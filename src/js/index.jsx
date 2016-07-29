6070b6f1a77f377731117888b6c7397c6c56fbe4
Robby Kim <robert.st.kim@gmail.com>
2016-07-29 15:12:09 -0400

    finished exercise

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -5,6 +5,7 @@ var router = require( 'react-router' );
 var Router = router.Router;
 var Route = router.Route;
 var hashHistory = router.hashHistory;
+var Link = router.Link;
 
 var CONTACTS = {
   0: {
@@ -28,7 +29,9 @@ var Contact = function( props ) {
   return (
     <div>
       <strong>
-        {props.name}
+        <Link to={'/contacts/' + props.id}>
+          {props.name}
+        </Link>
       </strong>
       &nbsp;
       {props.phoneNumber}
@@ -57,10 +60,35 @@ var ContactListContainer = function() {
   return <ContactList contacts={CONTACTS} />;
 };
 
+var IndexRoute = router.IndexRoute;
+
+var App = function(props) {
+  return (
+    <div>
+        <h1>
+          Contacts App
+        </h1>
+        <div>
+          {props.children}
+        </div>
+    </div>
+  );
+};
+
+var ContactContainer = function(props) {
+  var contact = CONTACTS[props.params.contactId];
+  return <Contact id={contact.id} name={contact.name}
+                  phoneNumber={contact.phoneNumber} />;
+}
+
+
 var routes = (
   <Router history={hashHistory}>
     <Route path="/" />
-    <Route path="/contacts" component={ContactListContainer} />
+    <Route path="/contacts" component={App}>
+      <IndexRoute component={ContactListContainer}/>
+      <Route path=":contactId" component={ContactContainer}/>
+    </Route>
   </Router>
 );
 
